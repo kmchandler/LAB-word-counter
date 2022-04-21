7c2db1d6e39f8cfafa5861955de09d4ec5d4ec91
Kristen Chandler <kristenchandler@kristens-mbp.lan>
2022-04-20 21:38:16 -0500

    completed dark mode challenge, and it's sexy

--- main.js
+++ main.js
@@ -16,9 +16,23 @@ const wordCounter = (value) => {
 }
 
 // OPTIONAL CHALLENGE
-const toggleMode = (btnText) => {
-  // complete the function
+const toggleMode = (evt) => {
+  const initialText = 'Dark Mode';
+  const btn = document.getElementById('bg-switch');
+  if (btn.textContent.toLowerCase().includes(initialText.toLowerCase())) {
+    btn.innerHTML = 'Light Mode';
+    document.body.classList.add("dark")
+    debugger;
+  } else {
+    btn.innerHTML = initialText;
+    document.body.classList.remove("dark")
+  }
 }
+
+const btn = document.getElementById('bg-switch');
+btn.addEventListener('click', toggleMode);
+  
+
 
 // ************************************************ //
 // **** DO NOT MODIFY THE CODE BELOW THIS LINE **** //
@@ -45,6 +59,6 @@ form.addEventListener("reset", () => {
   wordCount.innerHTML = ""; // on reset, clear the innderHTML
 });
 
-toggleButton.addEventListener("click", (event) => {
-  toggleMode(event.target.innerHTML);
-});
+// toggleButton.addEventListener("click", (event) => {
+//   toggleMode(event.target.innerHTML);
+// });
